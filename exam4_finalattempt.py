4536222b358f34e87bfc0871493a6645a5441c7c
Creslin Sena <creslin.sena019@gmail.com>
2024-05-08 19:35:16 -0400

    origin

--- exam4_finalattempt.py
+++ exam4_finalattempt.py
@@ -1,7 +1,7 @@
 #EXAM 4 CSENA
 #!/usr/bin/env python3
 
-def kmers_and_subskmers(genome_seq): #remember to indent everything
+def kmers_and_subskmers(genome_seq): #ignore how disgustingly long this function is
   """
   This is a function to find all kmers within a genome sequence, given that k = 2
   Function will also find all unique subsequent kmers of each kmer
@@ -15,7 +15,7 @@ def kmers_and_subskmers(genome_seq): #remember to indent everything
     list of lists containing each kmer and each unique subsequent kmer
   
   """
-  import re
+  import re #allows me to use re.finditer() later
   k = 2
   genome_length = list(range(len(genome_seq))) #the nested loops go crazy
   kmer_list = []
@@ -30,17 +30,41 @@ def kmers_and_subskmers(genome_seq): #remember to indent everything
             subs_kmer = genome_seq[p.start()+1:p.end()+1]
             if [kmer, subs_kmer] not in item2_list2:
               if len(subs_kmer) == k:
-                for index in range(k):
-                  if subs_kmer[index] != ' ': #\n STILL SHOWS UP, fuck it we ball
-                    item2_list2.append([kmer, subs_kmer])
+                item2_list2.append([kmer, subs_kmer])
               else:
-                if len(kmer) == k:
+                if len(kmer) == k: #makes sure to include kmers that dont have a full-length subs_kmer
                   item2_list2.append([kmer])
-                  print(item2_list2)
-
+                  with open("output_kmerssubs.txt", "a") as file67: #67 is a random number, i just dont want overlap with other variable names
+                    for apple in item2_list2: #im getting creative with variable names
+                      file67.write(",".join(map(str, apple)) + "\n") #PERFECT
+                      
 def kmers_and_subs_manylines(path):
+  """
+  This function will find all kmers and unique subsequent kmers, given that k=2, within 
+  a file containing multiple genome sequences.
+  Note: this will take a whie to run depending on size of file being processed, may result in disturbingly large output
+  
+  path (path): this is the path to the file containing the genome sequences that will be processed
+  
+  Return:
+    Output file "output_kmerssubs.txt" with each kmer and one of its subsequent kmers on each line
+    (if you already have a file named "output_kmerssubs.txt" from using kmers_and_subskmers(), it will be overwritten)
+  """
+  sequence = ""
+  part2_list = []
   with open(path, 'r') as file:
     for line in file:
-      kmers_and_subskmers(line) #ok it works, but why does it have \n line breaks
-      #omg does it think that \n is a fucking character, does it think the line break is a character
-      #LMAO just work on part 3
+      if line.strip() not in sequence:
+        sequence += line.strip() #turning all lines into a single sequence
+        print(sequence) #could potentially make a .txt output here to show what sequence actually looks like going into the main function
+        kmers_and_subskmers(sequence) #returns repeats for some reason
+          
+def find_k(path33): #this is how the code for item 3 should theoretically work, but who knows if it actually will
+  kmers_and_subs_manylines(path33)
+  for coconut in range(len(item2_list2)):
+    lime = coconut + 1
+    if item2_list2[coconut:coconut+k] = item2_list2[lime:lime+k]:
+      k += 1
+      kmers_and_subs_manylines(path33)
+      if item2_list2[coconut:coconut+k] != item2_list2[lime:lime+k]:
+        print(k)
